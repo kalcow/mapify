b5f0891a3b44c82be70e9d3e4ad070f528e6eba8
clairezhang03 <clairezhang03@gmail.com>
2023-02-21 18:23:16 -0800

    greeting, dropdown picker

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,49 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, {useState, FC} from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React, { useState, useEffect, FC } from 'react'
+import DropDownPicker from 'react-native-dropdown-picker';
+import NetInfo from '@react-native-community/netinfo'; //uninstall later
 
-interface UserInfo{
+//npm i react-native-dropdown-picker
+//npx expo install @react-native-community/netinfo
+
+interface UserInfo {
     name: string;
 }
 
 let hours = new Date().getHours();
 
-const Header: FC<UserInfo> = ({name}:UserInfo) => {
+const Header: FC<UserInfo> = ({ name }) => {
+    const [isOnline, setIsOnline] = useState(true);
+
+    //information for dropdown picker when online
+    const [openOn, setOpenOn] = useState(false);   //state variable that specifies whether the picker is open
+    const [valueOn, setValueOn] = useState('online');
+    const [itemsOn, setItemsOn] = useState([
+        { label: 'Online', value: 'online' },
+        { label: 'Ghost', value: 'ghost' },
+    ]);
+
+     //information for dropdown picker when offline
+     const [openOff, setOpenOff] = useState(false);   //state variable that specifies whether the picker is open
+     const [valueOff, setValueOff] = useState('offline');
+     const [itemsOff, setItemsOff] = useState([
+         { label: 'Offline', value: 'offline' }
+     ]);
+
+     
+    useEffect(() => {
+        // Subscribe to network state updates
+        const unsubscribe = NetInfo.addEventListener(state => {
+            if(state.isConnected != null)
+                setIsOnline(state.isConnected);
+          });
+    
+        return () => {
+          // Unsubscribe to network state updates
+          unsubscribe();
+        };
+
+      }, []);
 
     let greeting = "Good Morning";
     if (hours >= 11 && hours <= 16) {
@@ -18,17 +54,57 @@ const Header: FC<UserInfo> = ({name}:UserInfo) => {
         greeting = "Good Night";
     }
 
-    return (
-        <View style={styles.headerWrapper}>
-            <View style={styles.greetingWrapper}>
+    //drop-down menu is not compatible with styles such as borderColor or backgroundColor in the containerStyle property
+    if(isOnline){
+        return (
+            <View style={styles.headerWrapper}>
                 <Text style={styles.greetingText}>{greeting}, {"\n" + name}</Text>
+    
+                <DropDownPicker
+                    open={openOn}
+                    value={valueOn}
+                    items={itemsOn}
+                    setOpen={setOpenOn}
+                    setValue={setValueOn}
+                    placeholder="hello"
+                    containerStyle={{
+                        maxWidth: '30%',
+                        minHeight: 10,
+                        flex: 1
+    
+                    }}
+                />
+                <Image source={require("../assets/favicon.png")} />
             </View>
-                <img src={require("C:\Users\clair\Documents\mapify\assets\splash.png")} />
-            <View> 
-                
+        )
+    }
+
+    else{
+        return (
+            <View style={styles.headerWrapper}>
+                <Text style={styles.greetingText}>{greeting}, {"\n" + name}</Text>
+    
+                <DropDownPicker
+                    open={openOff}
+                    value={valueOff}
+                    items={itemsOff}
+                    setOpen={setOpenOff}
+                    setValue={setValueOff}
+                    placeholder="goodbye"
+                    containerStyle={{
+                        maxWidth: '30%',
+                        minHeight: 10,
+                        flex: 1
+    
+                    }}
+                    disabled={true}  //conditional component with only offline which is disabled
+    
+                />
+                <Image source={require("../assets/favicon.png")} />
             </View>
-        </View>
-    )
+        )
+    }
+ 
 }
 
 const styles = StyleSheet.create({
@@ -37,19 +113,12 @@ const styles = StyleSheet.create({
         color: "#FFF9EB"
     },
     headerWrapper: {
-        padding: 20
-    },
-
-    greetingWrapper: {
+        padding: 20,            //pads header on top and bottom
+        alignSelf: "stretch",   //stretches out to fill out horizontally
         backgroundColor: "#0E487D",
-        borderRadius: 200,
-        padding: 20,
-        paddingLeft: 30,
-        paddingRight: 30,
         display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignContent: "center",
+        flexDirection: "row",   //children are laid out in a row
+        justifyContent: "space-between",  //evenly space children 
     },
 })
 
