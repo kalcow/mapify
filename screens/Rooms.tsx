cd5a6ddacdcfa24c1032720c580eedd013b57665
maxgonick <maxwellgonick@g.ucla.edu>
2023-02-25 12:56:05 -0800

    Wireframe of button to generate room code

--- screens/Rooms.tsx
+++ screens/Rooms.tsx
@@ -2,14 +2,16 @@ import { getAccessToken } from '../lib/spotify';
 import { supabase } from '../supabase/supabase';
 import { useUserState } from '../context/user';
 
-import { View, Text } from 'react-native';
-import React, { useEffect } from 'react';
+import { View, Text, Button, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import Crypto from 'crypto';
+import { TextInput } from 'react-native-gesture-handler';
 
-type Props = {};
+type Room_Props = {};
 
-const Rooms = (props: Props) => {
+const Rooms = (props: Room_Props) => {
     const u = useUserState();
-
+    const [text, setText] = useState('Press to generate Room Code');
     useEffect(() => {
         const addKey = async () => {
             const { data, error } = await supabase.from('Rooms').insert([
@@ -26,10 +28,36 @@ const Rooms = (props: Props) => {
     }, []);
 
     return (
-        <View>
-            <Text style={{ color: 'white' }}>Rooms</Text>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'white',
+                flexDirection: 'column',
+            }}>
+            <Text>Rooms</Text>
+            <Text style={{ color: 'white' }}>Hi Max</Text>
+            <TouchableOpacity
+                style={{
+                    backgroundColor: 'grey',
+                    borderColor: 'black',
+                    padding: 10,
+                    borderRadius: 7,
+                }}
+                onPress={() => {
+                    useEffect(() => {});
+                }}>
+                <Text>{text}</Text>
+            </TouchableOpacity>
         </View>
     );
+};
+
+type Card_Props = {};
+
+const Card = (props: Card_Props) => {
+    return <View></View>;
 };
 
 export default Rooms;
