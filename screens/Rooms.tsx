f27ed95cf3ccdf28e3de61c15fbba2afdd0734e3
Edward Ng <3dward.ng@gmail.com>
2023-03-07 17:30:34 -0800

    Framework for Rooms Cards

--- screens/Rooms.tsx
+++ screens/Rooms.tsx
@@ -2,7 +2,7 @@ import { getAccessToken } from '../lib/spotify';
 import { supabase } from '../supabase/supabase';
 import { useUserState } from '../context/user';
 
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet, Dimensions, Image, FlatList} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import Crypto from 'crypto';
 import { TextInput } from 'react-native-gesture-handler';
@@ -11,7 +11,10 @@ type Room_Props = {};
 
 const Rooms = (props: Room_Props) => {
     const u = useUserState();
-    const [text, setText] = useState('Press to generate Room Code');
+    const [text, setText] = useState('+');
+    const [numRooms, setNumRooms] = useState(0);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [joinCode, setJoinCode] = useState("Enter Room Code")
     useEffect(() => {
         const addKey = async () => {
             const { data, error } = await supabase.from('Rooms').insert([
@@ -23,33 +26,40 @@ const Rooms = (props: Room_Props) => {
 
             console.log(data);
         };
-
-        addKey();
+        // addKey();
     }, []);
 
     return (
         <View
             style={{
                 flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: 'white',
+                paddingTop: 70,
+                backgroundColor: '#08080A',
                 flexDirection: 'column',
             }}>
-            <Text>Rooms</Text>
-            <Text style={{ color: 'white' }}>Hi Max</Text>
-            <TouchableOpacity
-                style={{
-                    backgroundColor: 'grey',
-                    borderColor: 'black',
-                    padding: 10,
-                    borderRadius: 7,
-                }}
-                onPress={() => {
-                    useEffect(() => {});
-                }}>
-                <Text>{text}</Text>
-            </TouchableOpacity>
+            <Text style={{color: 'white', fontSize: 30, alignItems: 'baseline', paddingLeft: 25}}>Rooms</Text>
+            <View style={{alignItems: 'center',}}>
+                <TextInput 
+                    onChangeText={setJoinCode}></TextInput>
+                <View style={{height: 3 * Dimensions.get('window').height * .2 + 60, overflow: "hidden"}}>
+                    <FlatList 
+                        data={[<Card></Card>, <Card></Card>, <Card></Card>, <Card></Card>]}
+                        renderItem={({item}) => item}
+                    />
+                </View>
+                <TouchableOpacity
+                    style={{
+                        backgroundColor: 'grey',
+                        borderColor: 'black',
+                        padding: 10,
+                        borderRadius: 7,
+                    }}
+                    onPress={() => {
+                        setModalVisible(true);
+                    }}>
+                    <Text>{text}</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -57,7 +67,54 @@ const Rooms = (props: Room_Props) => {
 type Card_Props = {};
 
 const Card = (props: Card_Props) => {
-    return <View></View>;
+    // This is placeholder name; will pass in props for the name at later point
+    const name = "Room name";
+    const icon = require('../assets/favicon.png')
+    const song_name_temp = "Die For You"
+    return( 
+        <View style={{paddingVertical: 10}}>
+            <View style={styles.roomCard}>
+                <View>
+                    <Text style={styles.title}>{name}</Text>
+                    <Text style={styles.subheading}>User-name</Text>
+                </View>
+                <View>
+                    <Image source={icon}/>
+                    <View>
+                        <Text style={{color: "white"}}>Currently Playing</Text>
+                        <Text style={{color: "white"}}>{song_name_temp}</Text>
+                    </View>
+                    <View>
+                        <Text style={{color: "white"}}>
+                            temp tag holder
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
 };
 
+const styles = StyleSheet.create({
+    roomCard: {
+        backgroundColor: "#1D1E24",
+        width:  Dimensions.get('window').width * .9,
+        height: Dimensions.get('window').height * .2,
+        borderRadius: 20,
+        padding: 25,
+    },
+    title: {
+        fontFamily: "Arial",
+        fontSize: 25,
+        color: "white",
+    },
+    subheading: {
+        fontFamily: "Arial",
+        fontSize: 17,
+        color: "white"
+    },
+    tags: {
+        flexDirection: 'row',
+    }
+})
 export default Rooms;
