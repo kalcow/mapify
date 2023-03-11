35b482fd712d8e59381e6dde9736ac97bbb00a58
maxgonick <maxwellgonick@g.ucla.edu>
2023-03-11 15:23:57 -0800

    Added refresh token support

--- screens/Rooms.tsx
+++ screens/Rooms.tsx
@@ -1,8 +1,19 @@
 import { getAccessToken } from '../lib/spotify';
 import { supabase } from '../supabase/supabase';
 import { useUserState } from '../context/user';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { View, Text, Button, TouchableOpacity, StyleSheet, Dimensions, Image, FlatList, Modal} from 'react-native';
+import {
+    View,
+    Text,
+    Button,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions,
+    Image,
+    FlatList,
+    Modal,
+} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { TextInput } from 'react-native-gesture-handler';
 import { setStatusBarBackgroundColor } from 'expo-status-bar';
@@ -16,15 +27,7 @@ const Rooms = (props: Room_Props) => {
     const [roomName, setName] = useState('');
     const [numRooms, setNumRooms] = useState(0);
     const [modalVisible, setModalVisible] = useState(false);
-    const [joinCode, setJoinCode] = useState("Enter Room Code")
-    useEffect(() => {
-        const addKey = async () => {
-            const { data, error } = await supabase.from('Rooms').insert([
-                {
-                    current_access_token: u.accessToken,
-                    current_users: 345,
-                },
-            ]);
+    const [joinCode, setJoinCode] = useState('Enter Room Code');
 
     const callApi = async () => {
         setText('loading...');
@@ -40,20 +43,27 @@ const Rooms = (props: Room_Props) => {
         }
     };
 
-    // useEffect(() => {
-    //     const addKey = async () => {
-    //         const { data, error } = await supabase.from('Rooms').insert([
-    //             {
-    //                 current_access_token: u.accessToken,
-    //                 current_users: 345,
-    //             },
-    //         ]);
+    const joinRoom = async () => {
+        const roomName = text;
+        const refreshToken = await AsyncStorage.getItem('@spotify_refresh_token');
+        try {
+            const response = await fetch('http://localhost:8080/createCode', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ room_name: roomName, refresh_token: refreshToken }),
+            });
+            const json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-    //         console.log(data);
-    //     };
-
-    //     addKey();
-    // }, []);
+    useEffect(() => {
+        console.log(text);
+    }, [text]);
 
     return (
         <View
@@ -63,32 +73,48 @@ const Rooms = (props: Room_Props) => {
                 backgroundColor: '#08080A',
                 flexDirection: 'column',
             }}>
-            <Text style={{color: 'white', fontSize: 30, alignItems: 'baseline', paddingLeft: 25}}>Rooms</Text>
-            <View style={{alignItems: 'center',}}>
-                <TextInput 
-                    onChangeText={setJoinCode}></TextInput>
-                <View style={{height: 3 * Dimensions.get('window').height * .2 + 60, overflow: "hidden"}}>
-                    <FlatList 
+            <Text style={{ color: 'white', fontSize: 30, alignItems: 'baseline', paddingLeft: 25 }}>
+                Rooms
+            </Text>
+            <TouchableOpacity
+                style={{
+                    backgroundColor: 'grey',
+                    borderColor: 'black',
+                    padding: 10,
+                    borderRadius: 7,
+                }}
+                onPress={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <Text>+</Text>
+            </TouchableOpacity>
+            <View style={{ alignItems: 'center' }}>
+                <TextInput onChangeText={setJoinCode}></TextInput>
+                <View
+                    style={{
+                        height: 3 * Dimensions.get('window').height * 0.2 + 60,
+                        overflow: 'hidden',
+                    }}>
+                    <FlatList
                         data={[<Card></Card>, <Card></Card>, <Card></Card>, <Card></Card>]}
-                        renderItem={({item}) => item}
+                        renderItem={({ item }) => item}
                     />
                 </View>
                 <Modal
                     visible={modalVisible}
                     animationType="slide"
                     onRequestClose={() => {
-                        setModalVisible(!modalVisible)
+                        setModalVisible(!modalVisible);
                     }}
-                    transparent={false}
-                >
+                    transparent={false}>
                     <View
-                        style={{paddingTop: 70, 
-                                alignContent:"center",
-                                backgroundColor: "#08080A",
-                                height: Dimensions.get('window').height,
-                                paddingLeft: 15
-                            }}
-                    >
+                        style={{
+                            paddingTop: 70,
+                            alignContent: 'center',
+                            backgroundColor: '#08080A',
+                            height: Dimensions.get('window').height,
+                            paddingLeft: 15,
+                        }}>
                         <TouchableOpacity
                             style={{
                                 padding: 10,
@@ -96,33 +122,27 @@ const Rooms = (props: Room_Props) => {
                             }}
                             onPress={() => {
                                 setModalVisible(!modalVisible);
-                            }}
-                        >
-                            <Text style={{color: 'white', fontSize: 30}}>
-                                {'<'}
-                            </Text>
+                            }}>
+                            <Text style={{ color: 'white', fontSize: 30 }}>{'<'}</Text>
                         </TouchableOpacity>
-                        <Text style={{color: "white", fontSize: 30, paddingLeft: 20}}> 
+                        <Text style={{ color: 'white', fontSize: 30, paddingLeft: 20 }}>
                             Create a Room
                         </Text>
                         <TextInput
-                            style={{backgroundColor: 'white', borderRadius: 50}}
-                            onChangeText={setName}
-                            value={roomName}
+                            style={{ backgroundColor: 'white', borderRadius: 50 }}
+                            onChangeText={setText}
                         />
-                        <View style={{alignItems: 'center'}}>
+                        <View style={{ alignItems: 'center' }}>
                             <TouchableOpacity
                                 style={{
                                     padding: 10,
                                     borderRadius: 50,
-                                    width: Dimensions.get('window').width * 1/2,
-                                    backgroundColor: "white",
+                                    width: (Dimensions.get('window').width * 1) / 2,
+                                    backgroundColor: 'white',
                                 }}
-                                onPress={() => {
-                                    // Add the create a room code here.
-                                }}
-                            >
-                                <Text style={{color: '#08080A', fontSize: 20, textAlign: "center"}}>
+                                onPress={joinRoom}>
+                                <Text
+                                    style={{ color: '#08080A', fontSize: 20, textAlign: 'center' }}>
                                     Create
                                 </Text>
                             </TouchableOpacity>
@@ -139,7 +159,7 @@ const Rooms = (props: Room_Props) => {
                     onPress={() => {
                         setModalVisible(!modalVisible);
                     }}>
-                    <Text>{text}</Text>
+                    <Text></Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -150,26 +170,24 @@ type Card_Props = {};
 
 const Card = (props: Card_Props) => {
     // This is placeholder name; will pass in props for the name at later point
-    const name = "Room name";
-    const icon = require('../assets/favicon.png')
-    const song_name_temp = "Die For You"
-    return( 
-        <View style={{paddingVertical: 10}}>
+    const name = 'Room name';
+    const icon = require('../assets/favicon.png');
+    const song_name_temp = 'Die For You';
+    return (
+        <View style={{ paddingVertical: 10 }}>
             <View style={styles.roomCard}>
                 <View>
                     <Text style={styles.title}>{name}</Text>
                     <Text style={styles.subheading}>User-name</Text>
                 </View>
                 <View>
-                    <Image source={icon}/>
+                    <Image source={icon} />
                     <View>
-                        <Text style={{color: "white"}}>Currently Playing</Text>
-                        <Text style={{color: "white"}}>{song_name_temp}</Text>
+                        <Text style={{ color: 'white' }}>Currently Playing</Text>
+                        <Text style={{ color: 'white' }}>{song_name_temp}</Text>
                     </View>
                     <View>
-                        <Text style={{color: "white"}}>
-                            temp tag holder
-                        </Text>
+                        <Text style={{ color: 'white' }}>temp tag holder</Text>
                     </View>
                 </View>
             </View>
@@ -179,24 +197,24 @@ const Card = (props: Card_Props) => {
 
 const styles = StyleSheet.create({
     roomCard: {
-        backgroundColor: "#1D1E24",
-        width:  Dimensions.get('window').width * .9,
-        height: Dimensions.get('window').height * .2,
+        backgroundColor: '#1D1E24',
+        width: Dimensions.get('window').width * 0.9,
+        height: Dimensions.get('window').height * 0.2,
         borderRadius: 20,
         padding: 25,
     },
     title: {
-        fontFamily: "Arial",
+        fontFamily: 'Arial',
         fontSize: 25,
-        color: "white",
+        color: 'white',
     },
     subheading: {
-        fontFamily: "Arial",
+        fontFamily: 'Arial',
         fontSize: 17,
-        color: "white"
+        color: 'white',
     },
     tags: {
         flexDirection: 'row',
-    }
-})
+    },
+});
 export default Rooms;
